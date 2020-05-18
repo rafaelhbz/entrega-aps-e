45c11217c101044ed3e609704f9184a0564c225b
Rafael <rafaelrhbz@gmail.com>
2020-05-18 20:55:35 -0300

    Aqui eu consegui passar em 3 testes, não sei se consigo chegar no 4 a tempo

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/XnorGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/XnorGate.java
@@ -22,6 +22,7 @@ public class XnorGate extends Gate {
         nand4.connect(0, nand2);
         nand4.connect(1, nand3);
         nand5.connect(0, nand4);
+        nand5.connect(1, nand4);
     }
 
     @Override
@@ -35,13 +36,11 @@ public class XnorGate extends Gate {
     public void connect(int inputIndex, Emitter emitter) {
         if (inputIndex == 0) {
             nand1.connect(0, emitter);
-            nand2.connect(1, emitter);
-            nand4.connect(0, emitter);
+            nand2.connect(0, emitter);
         }
         if (inputIndex == 1) {
             nand1.connect(0, emitter);
-            nand3.connect(0, emitter);
-            nand4.connect(1, emitter);
+            nand3.connect(1, emitter);
         }
     }
 
